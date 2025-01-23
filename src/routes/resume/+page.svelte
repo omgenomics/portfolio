18a7543be4054e81e5a227ef8b0f20b86e42ff34
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2025-01-23 14:50:23 -0800

    Add more example content on resume page

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -13,7 +13,7 @@ import { ListGroup } from "@sveltestrap/sveltestrap";
 				<div class="fw-bold">
 					Senior Bioinformatics Scientist <span class="fw-normal text-muted small">(Jan 2024 &mdash;)</span>
 				</div>
-				Led effort to reduce variant calling error rates by 20% in our ABC pipeline, ...
+				Led effort to reduce variant calling error rates by 20% in our ABC pipeline ...
 			</li>
 			<li>
 				<div class="fw-bold">
@@ -23,11 +23,25 @@ import { ListGroup } from "@sveltestrap/sveltestrap";
 			</li>
 		</ul>
 	</Experience>
-	<Experience start="January 2017" end="March 2022" company="XYZ Bio" url="https://google.com"></Experience>
+
+	<Experience start="January 2017" end="March 2022" company="XYZ Bio" url="https://google.com">
+		<ul class="timeline">
+			<li>
+				<div class="fw-bold">Bioinformatics Scientist</div>
+				Built pipelines for analyzing XYZ...
+			</li>
+		</ul>
+	</Experience>
 </ListGroup>
 
 <SectionHeader title="Education" />
 
 <ListGroup flush>
-	<Experience start="April 2013" end="January 2017" company="ABC Bio" url="https://google.com" />
+	<Experience start="August 2013" end="January 2017" company="Graduate School of Biology" url="https://google.com">
+		<div class="fw-bold">PhD in Biology</div>
+		Studied the effect of XYZ...
+	</Experience>
+	<Experience start="September 2009" end="August 2013" company="School of Biological Biology" url="https://google.com">
+		<div class="fw-bold">B.Sc. in Biology</div>
+	</Experience>
 </ListGroup>
