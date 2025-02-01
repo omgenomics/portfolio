57024a41a688c51610a6bed8d2ea17d328f1d72f
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2025-02-01 15:34:20 -0800

    Add Markdown blog support

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,17 @@
 import adapter from "@sveltejs/adapter-cloudflare";
+import { mdsvex } from "mdsvex";
 
 const config = {
 	kit: {
 		adapter: adapter()
-	}
+	},
+	extensions: [".svelte", ".md"],
+	preprocess: [
+		mdsvex({
+			extensions: [".md"],
+			layout: "./src/components/BlogPost.svelte"
+		})
+	]
 };
 
 export default config;
