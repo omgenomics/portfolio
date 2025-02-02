2a57ba1e5dbd72096da402e714e184053c387817
Robert Aboukhalil <robert.aboukhalil@gmail.com>
2025-02-01 20:26:36 -0800

    Fix eslint config

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import prettier from "eslint-config-prettier";
+import js from "@eslint/js";
+import { includeIgnoreFile } from "@eslint/compat";
+import svelte from "eslint-plugin-svelte";
+import globals from "globals";
+import { fileURLToPath } from "node:url";
+const gitignorePath = fileURLToPath(new URL("./.gitignore", import.meta.url));
+
+export default [
+	includeIgnoreFile(gitignorePath),
+	js.configs.recommended,
+	...svelte.configs["flat/recommended"],
+	prettier,
+	...svelte.configs["flat/prettier"],
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node
+			}
+		}
+	}
+];
